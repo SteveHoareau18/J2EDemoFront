040caa3a4667504754066dacfff32cd83558cbec
Steve <steve.hoareau1@gmail.com>
2024-02-27 14:38:51 +0200

    css and get user

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,36 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from "svelte";
+
+  let responseData;
+
+  async function fetchData() {
+    try {
+      const response = await fetch("http://localhost:8080/user/list");
+      console.log(response);
+      responseData = await response.json();
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="body text-center">
+    <div class="nav">
+      <h1>User Manager</h1>
+    </div>
+    <div class="flex justify-around">
+      <button on:click={fetchData}>List</button>
+      <a href="#">Add</a>
+    </div>
+    <div>
+      {#if responseData}
+        <ul>
+          {#each responseData as item}
+            <li>{item.name}</li>
+          {/each}
+        </ul>
+      {/if}
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
