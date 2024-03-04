cd7a1bbe780fa110ce6579a1c0ebbdb58a0367bf
Steve <steve.hoareau1@gmail.com>
2024-03-04 10:29:09 +0400

    env file

--- src/App.svelte
+++ src/App.svelte
@@ -28,7 +28,7 @@
 
 <main>
   <div class="body text-center">
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <nav class="border-gray-200 bg-gray-900">
       <div
         class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
       >
@@ -42,18 +42,18 @@
             alt="Flowbite Logo"
           />
           <span
-            class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+            class="self-center text-2xl font-semibold whitespace-nowrap text-white"
             >GestCom!</span
           >
         </a>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul
-            class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+            class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700"
           >
             {#each Object.entries(pages) as [page, details]}
               <li>
                 <button
-                  class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  class="block py-2 px-3 rounded md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent"
                   aria-current="page"
                   on:click|preventDefault={() => active(page)}
                   >{details.name}</button
